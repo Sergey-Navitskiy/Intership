f45d6c77a88380bf15050d5fe22f8a91b23c0c11
Sergey-Navitskiy <126409337+Sergey-Navitskiy@users.noreply.github.com>
2025-09-18 13:20:04 +0300

    Update script.js

--- Week-1/ArraySecondMax/script.js
+++ Week-1/ArraySecondMax/script.js
@@ -8,5 +8,6 @@ function searchSecondMaxNumber(array) {
 
   return array[1];
 }
-
+const array1 = [2, 3, 4, 8, 98, 67, 54, 33, 22, 7, 12, 323];
 console.log(searchSecondMaxNumber([14, 5, 2, 12, 10]));
+console.log(searchSecondMaxNumber(array1));
